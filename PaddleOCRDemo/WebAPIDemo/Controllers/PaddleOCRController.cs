c1ae1e9814f3288e5d33eedc3c5ddb8fedd4e613
Yutian Rao <72854228+raoyutian@users.noreply.github.com>
2022-03-22 22:02:03 +0800

    Add files via upload

---
+++ PaddleOCRDemo/WebAPIDemo/Controllers/PaddleOCRController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaddleOCRSharp;
+using System;
+using System.Drawing;
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaddleOCRController : ControllerBase
+    {
+
+        //[HttpGet]
+        //public ViewResult Get()
+        //{
+        //    return new ViewResult();
+        //}
+
+        [HttpPost("DetectText")]
+        public string DetectText([FromBody]string base64)
+        {
+            var imagebyte = Convert.FromBase64String(base64);
+            Bitmap bitmap = new Bitmap(new System.IO.MemoryStream(imagebyte));
+            var result = PaddleOCRHelper.Instance.DetectText(bitmap);
+            Console.WriteLine(result.Text);
+            return result.Text;
+        }
+    }
+}
